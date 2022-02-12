351b6a3f6c9b690ed0733e286fc973c8f34594d9
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-02-12 16:14:11 +0100

    Added php
    added php for site to work better

--- php/components/navbar.php
+++ php/components/navbar.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Dabay</title>
-  <link rel="stylesheet" href="./style/css/style.css">
-</head>
-
-<body class="dark">
-  <nav class="main-nav">
-    <ul>
+<ul>
       <li>
         <h3 class="text-logo">Dabay</h3>
       </li>
@@ -65,7 +52,3 @@
         </ul>
       </li>
     </ul>
-  </nav>
-</body>
-
-</html>
