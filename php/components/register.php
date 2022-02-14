975d3bc2da20607baa6ea47418ca20cce504e2f5
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-02-14 17:31:36 +0100

    Change: Added login/register.php

---
+++ php/components/register.php
@@ -0,0 +1,22 @@
+<div class="test">
+<div class="register">
+    <h1>SignUp</h1>
+    <form action="./create-user.php" method="post">
+      <div class="inputs">
+        <input class="input" type="text" name="user" class="" value="" placeholder="Name">
+
+        <input class="input" type="text" name="username" class="" value="" placeholder="Username">
+
+        <input class="input" type="password" name="password" class="" value="" placeholder="Password">
+
+        <input class="input" type="email" name="email" class="" value="" placeholder="E-mail">
+      </div>
+
+      <input class="btn-1" type="submit" value="SignUp">
+    </form>
+    <div class="login">
+      <p>You are a member? <a <?php echo ($active == "login" )? "active" : "" ?>" href="index.php?content=./php/components/login">Login</a></p>
+      <p>Stay as <a <?php echo ($active == "home" )? "active" : "" ?>" href="index.php?content=./php/content/home">Guest</a></p>
+    </div>
+  </div>
+  </div>
