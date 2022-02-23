f7a041399c4e06b1053c6090db710847d1b659ee
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-02-23 15:54:24 +0100

    Change: Added is authorized function

--- php/scripts/functions.php
+++ php/scripts/functions.php
@@ -6,3 +6,14 @@ function sanitize($raw_data) {
       $data = htmlspecialchars($data);
       return $data;
     }
+
+
+    function is_authorized($userroles) {
+      if (!isset($_SESSION["id"])) {
+        return header("Location: ./index.php?content=message&alert=auth-error");
+      } elseif (!in_array($_SESSION["userrole"], $userroles)) {
+        return header("Location: ./index.php?content=message&alert=auth-error-userrole");
+      } else {
+        return true;
+      }
+    }
