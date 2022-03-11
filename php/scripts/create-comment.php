ba3cf5a7de392b6cbe56795afc7dc3f30cd89635
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-03-11 14:02:08 +0100

    Change: fixed a comment bug
    now ppl who are not logged in cannot comment and will not get an error message

--- php/scripts/create-comment.php
+++ php/scripts/create-comment.php
@@ -2,9 +2,13 @@
   include("connect-db.php");
   include("functions.php");
 
+  if (!isset($_SESSION["userID"])) {
+    header("Location: index.php?content=php/content/home");
+  } else {
   $postID = sanitize($_POST['postID']);
   $user = $_SESSION["userID"];
   $comment = sanitize($_POST['commentText']);
   $sql = "INSERT INTO `comments` (`commentID`, `postID`, `userID`, `comment`) VALUES (NULL, '$postID', '$user', '$comment')";
   $comments = mysqli_query($conn, $sql);
   header("Location: index.php?content=php/content/home#". $_POST["elementID"] ."");
+  }
