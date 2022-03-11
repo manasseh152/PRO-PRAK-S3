b286a7223ac688870b9a4409e57cc570a64ca7fd
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-03-11 16:05:59 +0100

    Change: unset session

--- php/scripts/logout.php
+++ php/scripts/logout.php
@@ -1,7 +1,8 @@
 <?php
 var_dump($_SESSION);
-  unset($_SESSION["id"]);
+  unset($_SESSION["userID"]);
   unset($_SESSION["userrole"]);
+  unset($_SESSION["username"]);
   var_dump($_SESSION);
   session_destroy();
   header("Location: index.php?content=php/components/register");
