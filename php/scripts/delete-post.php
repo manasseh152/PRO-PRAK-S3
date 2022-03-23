b0d98df467d2583533a50952d760368f1dc3b307
Raoul-van-Wijk <94360844+Raoul-van-Wijk@users.noreply.github.com>
2022-03-23 16:37:04 +0100

    Change: Changed post
    changed post so that only admins or owners of post can update and delete the post

--- php/scripts/delete-post.php
+++ php/scripts/delete-post.php
@@ -1,8 +1,9 @@
 <?php
   include('php/scripts/functions.php');
+  is_authorized('user', 'admin', 'root', 'moderator');
   $postID = sanitize($_POST["postID"]);
   $userID = $_SESSION['userID'];
-  $sql = "DELETE FROM post WHERE `post`.`postID` = $postID AND `post`.`userID` = $userID";
+  $sql = "DELETE FROM post WHERE `post`.`postID` = $postID";
   runStatement($sql);
   var_dump($_GET["content"]);
   header('Location: index.php?content='. $_POST['URL'] .'');
--- php/scripts/functions.php
+++ php/scripts/functions.php
@@ -59,25 +59,28 @@ function showPost($sql)
                     <path d="M2 0C0.9 0 0 0.9 0 2C0 3.1 0.9 4 2 4C3.1 4 4 3.1 4 2C4 0.9 3.1 0 2 0ZM14 0C12.9 0 12 0.9 12 2C12 3.1 12.9 4 14 4C15.1 4 16 3.1 16 2C16 0.9 15.1 0 14 0ZM8 0C6.9 0 6 0.9 6 2C6 3.1 6.9 4 8 4C9.1 4 10 3.1 10 2C10 0.9 9.1 0 8 0Z"/>
                     </svg>
                   </button>
-                  <ul>
-                    <li>
-                      <form action="index.php?content=php/content/updatepost" method="post">
-                        <input type="hidden" name="postID" value=' . $record["postID"] . '>
-                        <input type="hidden" name="URL" value='. $_GET["content"] .'>
-                        <button type="submit">
-                          Update
-                        </button>
-                      </form>
-                    </li>
-                    <li>
-                      <form action="index.php?content=php/scripts/delete-post" method="post">
-                        <input type="hidden" name="postID" value=' . $record["postID"] . '>
-                        <input type="hidden" name="URL" value='. $_GET["content"] .'>
-                        <button type="submit">
-                          Delete
-                        </button>
-                      </form>
-                    </li>
+                  <ul>';
+                  if($record["username"] == $_SESSION["username"] || $_SESSION["userrole"] == "admin") {
+                    echo '<li>
+                    <form action="index.php?content=php/content/updatepost" method="post">
+                      <input type="hidden" name="postID" value=' . $record["postID"] . '>
+                      <input type="hidden" name="URL" value='. $_GET["content"] .'>
+                      <button type="submit">
+                        Update
+                      </button>
+                    </form>
+                  </li>
+                  <li>
+                    <form action="index.php?content=php/scripts/delete-post" method="post">
+                      <input type="hidden" name="postID" value=' . $record["postID"] . '>
+                      <input type="hidden" name="URL" value='. $_GET["content"] .'>
+                      <button type="submit">
+                        Delete
+                      </button>
+                    </form>
+                  </li>';
+                  }echo'
+                    
                     <li><a herf="#">lees pop</a></li>
                     <li><a herf="#">lees rel</a></li>
                   </ul>
