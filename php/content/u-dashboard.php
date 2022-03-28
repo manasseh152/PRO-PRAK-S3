b2a85b1809cedc221b4e0bc598a769aac628f382
manasseh152 <61059525+manasseh152@users.noreply.github.com>
2022-03-28 20:39:47 +0200

    User dash board fix

--- php/content/u-dashboard.php
+++ php/content/u-dashboard.php
@@ -1,36 +1,51 @@
 <?php
-  include("php/scripts/functions.php");
-  is_authorized(['root', 'admin', 'moderator', 'user']);
+include("php/scripts/functions.php");
+is_authorized(['root', 'admin', 'moderator', 'user']);
 
-  // userID verkrijgen via session
-  $user = $_SESSION['userID'];
+// userID verkrijgen via session
+$user = $_SESSION['userID'];
 
-  // alle posts user
-  $sql = "SELECT COUNT(`postID`) AS `totalUserPost` FROM `post` WHERE `userID` = $user";
-  $totalUserPost = runStatement($sql);
-  $totalUserPost = mysqli_fetch_assoc($totalUserPost);
-  echo $totalUserPost["totalUserPost"];
+// alle posts user
+$sql = "SELECT COUNT(`postID`) AS `totalUserPost` FROM `post` WHERE `userID` = $user";
+$totalUserPost = runStatement($sql);
+$totalUserPost = mysqli_fetch_assoc($totalUserPost);
 
-  // alle upvote naar user
-  $sql = "SELECT SUM(`upvote`) as `totalUpvotes` FROM `post` WHERE `userID` = $user";
-  $totalUpvotes = runStatement($sql);
-  $totalUpvotes = mysqli_fetch_assoc($totalUpvotes);
-  // echo $totalUpvotes["totalUpvotes"];
 
-  // totaal comments van user
-  $sql = "SELECT COUNT(`commentID`) as `totalComments` FROM `comments` WHERE `userID` = $user";
-  $totalComments = runStatement($sql);
-  $totalComments = mysqli_fetch_assoc($totalComments);
-  // echo $totalComments["totalComments"];
+// alle upvote naar user
+$sql = "SELECT SUM(`upvote`) as `totalUpvotes` FROM `post` WHERE `userID` = $user";
+$totalUpvotes = runStatement($sql);
+$totalUpvotes = mysqli_fetch_assoc($totalUpvotes);
 
-  // alle posts
-  $sql = "SELECT COUNT(`postID`) AS `totalPost` FROM `post`";
-  $totalPost = runStatement($sql);
-  $totalPost = mysqli_fetch_assoc($totalPost);
-  // echo $totalPost["totalPost"];
-  if ($totalUserPost["totalUserPost"] = 0) {
-    echo "niet mogelijk";
-  } else {
-  $percental =  $totalUserPost["totalUserPost"] / $totalPost["totalPost"] * 100;}
-  // echo $percental
+
+// totaal comments van user
+$sql = "SELECT COUNT(`commentID`) as `totalComments` FROM `comments` WHERE `userID` = $user";
+$totalComments = runStatement($sql);
+$totalComments = mysqli_fetch_assoc($totalComments);
+
+
+// alle posts
+$sql = "SELECT COUNT(`postID`) AS `totalPost` FROM `post`";
+$totalPost = runStatement($sql);
+$totalPost = mysqli_fetch_assoc($totalPost);
+if ($totalUserPost["totalUserPost"] = 0) {
+  echo "niet mogelijk";
+} else {
+  $percental =  $totalUserPost["totalUserPost"] / $totalPost["totalPost"] * 100;
+}
+// echo $percental
 ?>
+
+<div class="dashboard">
+  <div class="u">
+    <p>user dashboard</p>
+  </div>
+  <div class="m">
+    <p><?= $totalUserPost["totalUserPost"] ?></p>
+    <p><?= $totalUpvotes["totalUpvotes"] ?></p>
+    <p><?= $totalComments["totalComments"] ?></p>
+    <p><?= $totalPost["totalPost"] ?></p>
+  </div>
+  <div class="p">
+
+  </div>
+</div>
